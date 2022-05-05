22aff6ea3b62450e02dd921b424f9cc8129db408
salmen1234 <105023745+salmen1234@users.noreply.github.com>
2022-05-06 01:10:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from os import kill
-from turtle import color
 import discord
 from discord.ext import commands
-from matplotlib.pyplot import title
 import requests
 from bs4 import BeautifulSoup
 
